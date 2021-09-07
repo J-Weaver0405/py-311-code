a42876805d00df2e1846b31141aa0ab60f1c6f1b
dd-code-immersives <domilamad89@gmail.com>
2021-09-07 12:31:36 -0400

    added solution to exercise 1

--- code-snippets/argv.py
+++ code-snippets/argv.py
@@ -2,4 +2,14 @@
 import sys
 
 print(f"Name of the script      : {sys.argv[0]=}")
-print(f"Arguments of the script : {sys.argv[1:]=}")
+
+args = sys.argv[1:]
+print(args) 
+
+# all(True, True, True) -> True 
+# all(False, True, True) -> False
+if all(arg.isdigit() for arg in args):
+	print(f"input was all digits, sum:{sum(int(arg) for arg in args)}")
+
+if all(arg.isascii() for arg in args):
+	print(f"input was all characters: { ','.join(args) }")
